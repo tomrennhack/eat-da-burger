1d6725067fcde5b1f961df0cabff8de923dec269
tomrennhack <tomrennhack@gmail.com>
2019-10-28 08:42:43 -0400

    pw change

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.PASS,
+  password: "Fartdog90s",
   database: "burgers_db"
 });
 
